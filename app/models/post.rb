c5466d92791f112032d2e7c308a5b759a6b54f7b
Kristian Andersen <kra@pentia.dk>
2023-08-23 09:47:52 +0200

    validation_posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 # Post class, to allow users to create posts
 class Post < ApplicationRecord
+  validates :title, presence: true, uniqueness: true , length: { maximum: 50 }
+  validates :body, presence: true
+  
   belongs_to :user
 end
