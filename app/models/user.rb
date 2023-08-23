8ecdc3357fc6b40c236a73e64c4a25f77a509bcd
Kristian Andersen <kra@pentia.dk>
2023-08-23 09:18:50 +0200

    ..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # User class with validations
 class User < ApplicationRecord
-    validations :email, presence: true, uniqueness: true, fomat: { with: URI::MailTo::EMAIL_REGEXP }
-    validations :username, presence: true, uniqueness: true, length: { in: 3..20 }
-    validations :password, presence: true, length: { in: 8..24 }
+    validates :email, presence: true, uniqueness: true, fomat: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+    validates :password, presence: true, length: { in: 8..24 }
 end
